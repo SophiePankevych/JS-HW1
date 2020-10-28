68197b384fd39eea2974f9273902ac3d97f4818a
Sophie Pankevych <sofiapankevych11@gmail.com>
2020-10-28 16:10:46 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,229 @@
+// - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7,
+//  16, true, false Вывести каждую при помощи console.log , alert, document.write
+// let a1 = 'hello';
+// let a2 = 'owu';
+// let a3 = 'com';
+// let a4 = 'ua';
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3.14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+// document.write(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13);
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// alert(a13);
+
+// - переопределить каждую переменную из задания 1 дав им произвольные значения
+// Вывести каждую при помощи console.log , alert, document.write
+// let a1 = 'sadmksnk';
+// let a2 = true;
+// let a3 = 'sdvd';
+// let a4 = 12214;
+// let a5 = 'wsgswd';
+// let a6 = false;
+// let a7 = jdnswl;
+// let a8 = 432;
+// let a9 = 'sdccv';
+// let a10 = true;
+// let a11 = 'lmombgonmdn';
+// let a12 = 432343;
+// let a13 = false;
+// document.write(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13);
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+// alert(a1);
+// alert(a2);
+// alert(a3);
+// alert(a4);
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// alert(a11);
+// alert(a12);
+// alert(a13);
+
+// - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+// const number1 = 123;
+// const number2 = 54346;
+// const number3 = 354325;
+// const str1 = 'rwerewre';
+// const str2 = 'grgnrong';
+// const str3 = 'ee';
+// document.write(number1, number2, number3, str1, str2, str3);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+
+// - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО.
+// Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+// let lastname = prompt('Enter your last name');
+// let firstname = prompt('Enter your first name');
+// let fathersname = prompt('Enter your father`s name');
+// document.write(lastname, firstname, fathersname);
+// console.log(lastname);
+// console.log(firstname);
+// console.log(fathersname);
+// alert(lastname);
+// alert(firstname);
+// alert(fathersname);
+
+// - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+// let lastname = prompt('Enter your last name');
+// let firstname = prompt('Enter your first name');
+// let fathersname = prompt('Enter your father`s name');
+// let person = `${lastname} ${firstname} ${fathersname}`;
+// console.log(person);
+
+// - Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+// let lastname = prompt('Enter your last name');
+// let firstname = prompt('Enter your first name');
+// let fathersname = prompt('Enter your father`s name');
+// let person = `${lastname} ${firstname} ${fathersname}`;
+// console.log(person);
+
+// - при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+// let num1 = +prompt('Enter first num');
+// let num2 = +prompt('Enter second num');
+// let num3 = +prompt('Enter third num');
+// console.log(num1 + num2 + num3);
+// console.log(typeof num1);
+// console.log(num1);
+
+// - при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt.
+// Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+// let num1 = +prompt('Enter first num');
+// let num2 = +prompt('Enter second num');
+// let num3 = +prompt('Enter third num');
+// let num4 = +prompt('Enter fourth num');
+// let int1 = parseInt(num1);
+// let int2 = parseInt(num2);
+// let int3 = parseInt(num3);
+// let int4 = parseInt(num4);
+// let result = int1 + int2 + int3 + int4;
+// console.log(result);
+
+// - при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему
+// типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+// let num1 = +prompt('Enter first floating num');
+// let num2 = +prompt('Enter second floating num');
+// let num3 = +prompt('Enter third floating num');
+// let float1 = parseFloat(num1);
+// let float2 = parseFloat(num2);
+// let float3 = parseFloat(num3);
+// let result = float1 + float2 + float3;
+// console.log(result);
+
+// - при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между
+// собой записав результат в переменную  и вывести в консоль браузера
+// let num1 = +prompt('Enter first flow num');
+// let num2 = +prompt('Enter second flow num');
+// let num3 = +prompt('Enter third flow num');
+// let round1 = Math.round(num1);
+// let round2 = Math.round(num2);
+// let round3 = Math.round(num3);
+// let result = round1 + round2 + round3;
+// console.log(result);
+
+// - при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое
+// будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое
+// число в степень второго числа.
+// let num1 = +prompt('Enter first num');
+// let num2 = +prompt('Enter second num');
+// let powNum = Math.pow(num1, num2);
+// console.log(powNum);
+
+// - При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+//     В одинаковых выражениях в задании не использовать два одинаковых оператора.
+//     Каждое выражение вложить в свою переменную
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 === 6 -> false
+// 5 >= 6 -> false
+// 10 === 10 -> true
+// 10 >= 10 -> true
+// 10 != 10 -> false
+// 10 < 10 -> false
+// 10 > 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+
+// Дополнительно:
+//     - Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+// console.log(132 > 100 && 45 < 12 ); ==> false
+// console.log(34 > 33 && 23 < 90 );  ==> true
+// console.log(99 > 100 && 45 > 12 );  ==> false
+// console.log(132 > 100 || 45 < 12 );  ==> true
+// console.log(111 > 11 || 45 < 111 );  ==> true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );  ==> true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );  ==> true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );  ==> false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));  ==> true
+// console.log(!!'-1');  ==> true
+// console.log(!!-1);  ==> true
+// console.log(!!'0');  ==> true
+// console.log(!!'null');  ==> true
+// console.log(!!'undefined'); ==> true
+// console.log(!!(3/'owu'));  ==> false
+// console.log((111 > 11 || 45 < 111) ||  !!'0');  ==> true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); ==> true
